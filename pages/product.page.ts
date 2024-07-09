5667ad0520d29aeaf76321613bf0e3e92c36012f
fayeem.mooktadeer <mmookta1@binghamton.edu>
2024-07-09 01:25:33 -0500

    Completed Playwright Exercise

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -1,8 +1,11 @@
-import { Page } from "@playwright/test"
+import { Page,expect } from "@playwright/test"
 
 export class Product {
     private readonly page: Page
-    private readonly addToCart: string = 'button[id="add-to-cart-sauce-labs-backpack"]'
+    private readonly addToCart: string = '[data-test="add-to-cart-sauce-labs-backpack"]'
+    private readonly cartBtn: string = '#shopping_cart_container';
+    private readonly sortDropDown:string = '[data-test="product-sort-container"]'
+    private readonly productPrice:string='[data-test="inventory-item-price"]'
 
     constructor(page: Page) {
         this.page = page;
@@ -10,5 +13,27 @@ export class Product {
 
     public async addBackPackToCart() {
         await this.page.locator(this.addToCart).click()
+    }
+    public async clickCart() {
+        await this.page.locator(this.cartBtn).click()
+    }
+    public async sortProduct(option: any){
+        await this.page.locator(this.sortDropDown).selectOption({label:option})
+    }
+    public async isSort(sortOption: string): Promise<void> {
+        const totalProducts = await this.page.locator(this.productPrice).count();
+        const prices: number[] = [];
+      
+        for (let i = 0; i < totalProducts; i++) {
+          const price =  await this.page.locator(this.productPrice).nth(i).textContent();
+          if (price) {
+            const finalPrice = parseFloat(price.replace('$', ''));
+            prices.push(finalPrice);
+          }
+        }
+        const sortedPrices = sortOption === 'Price (low to high)' 
+          ? [...prices].sort((a, b) => a - b)
+          : [...prices].sort((a, b) => b - a);
+        expect(prices).toEqual(sortedPrices);
     }
 }
--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,19 @@
-import { Then } from '@cucumber/cucumber';
+import { Then,When } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
 
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+
+Then('I can select the cart', async () => {
+  await new Product(getPage()).clickCart()
+});
+
+When(/^I sort items by (Price \(high to low\)|Price \(low to high\))$/, async function (sortOption) {
+  await new Product(getPage()).sortProduct(sortOption)
+});
+
+Then(/^I validate all 6 items are sorted correctly by price (Price \(high to low\)|Price \(low to high\))$/, async function (sortOption) {
+await new Product(getPage()).isSort(sortOption)
 });
